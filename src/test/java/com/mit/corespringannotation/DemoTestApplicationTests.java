3d9e7ecaf011e8ecc44d3e1f568d8abc67fef1b7
rohitdevikar <rohit.devikar@avcoe.org>
2023-07-13 09:45:37 +0100

    Initial Commit

---
+++ src/test/java/com/mit/corespringannotation/DemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mit.corespringannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
